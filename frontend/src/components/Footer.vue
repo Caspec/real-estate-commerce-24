1ac5dee253d80c53c3fbe3dce5d53309e5d30911
Caspec <quii2000@yahoo.dk>
2021-02-02 15:28:30 +0100

    frontend work

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-footer color="grey lighten-3" padless>
+    <v-row no-gutters>
+      <v-col text class="my-2 pl-4" 
+        ><span class="font-weight-medium">Find real estate agent</span> 
+        <v-row no-gutters><v-col>Agent1</v-col></v-row>
+        <v-row no-gutters><v-col>Agent2</v-col></v-row>
+        <v-row no-gutters><v-col>Agent3</v-col></v-row>
+      </v-col>
+      <v-col text class="my-2 pl-2"
+        ><span class="font-weight-medium">Buy and sell residential</span>
+        <v-row no-gutters><v-col>Project sales & rentals</v-col></v-row>
+        <v-row no-gutters><v-col>REC24 buyer advice</v-col></v-row>
+        <v-row no-gutters><v-col>Parental purchase</v-col></v-row>
+      </v-col>
+      <v-col text class="my-2 pl-2"
+        ><span class="font-weight-medium">News about the residential market</span>
+        <v-row no-gutters><v-col>Residential news</v-col></v-row>
+        <v-row no-gutters><v-col>Subscribe to newsletter</v-col></v-row>
+        <v-row no-gutters><v-col>Press</v-col></v-row>
+      </v-col>
+      <v-col text class="my-2 pl-2"
+        ><span class="font-weight-medium">About REC24</span>
+        <v-row no-gutters><v-col>About REC24</v-col></v-row>
+        <v-row no-gutters><v-col>Job at REC24</v-col></v-row>
+        <v-row no-gutters><v-col>Become a franchisee</v-col></v-row>
+      </v-col>
+      <v-col class="grey lighten-2 py-4 text-center black--text" cols="12">
+        {{ new Date().getFullYear() }} — <strong>Real Estate Commerce 24</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data() {
+
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <v-app-bar app color="primary">
+      <div class="d-flex align-center white--text">Real Estate Commerce 24</div>
+      <v-spacer class="hidden-xs-only"></v-spacer>
+      <v-form>
+        <v-autocomplete
+          chips
+          clearable
+          hide-details
+          hide-selected
+          item-text="name"
+          item-value="estate"
+          label="Search for a real estate..."
+          solo
+          class="light-blue lighten-5"
+        >
+        </v-autocomplete>
+      </v-form>
+      <v-app-bar-nav-icon class="white--text" @click="drawer = true"></v-app-bar-nav-icon>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute temporary right>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      group: null,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
